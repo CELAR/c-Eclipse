7c945eef4ff586a4979f693e105601dcd42b0fb8
Nicholas Loulloudes <nickl@cs.ucy.ac.cy>
2013-06-17 12:57:59 +0300

    First Commit for TestGit project

---
+++ app-description/eu.celar.testgit/src/TestGitProject.java
@@ -0,0 +1,21 @@
+
+/************************************************************
+ * Copyright (C), 2013 CELAR Consortium
+ * http://www.celarcloud.eu
+ *
+ * Contributors:
+ *      Nicholas Loulloudes - initial API and implementation 
+ ************************************************************/
+/**
+ * @author Nicholas Loulloudes
+ *
+ */
+public class TestGitProject {
+
+  /**
+   * @param args
+   */
+  public static void main( final String[] args ) {
+    System.out.println("Hello Git Project"); //$NON-NLS-1$
+  }
+}
