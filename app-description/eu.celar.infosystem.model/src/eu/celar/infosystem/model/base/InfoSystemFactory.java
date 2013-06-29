ec5b4520372b8e9ee8359112d95b0329a65fdcfe
Nicholas Loulloudes <nickl@cs.ucy.ac.cy>
2013-06-29 11:35:55 +0300

    First commit of eu.celar.infosystem.mockup

---
+++ app-description/eu.celar.infosystem.model/src/eu/celar/infosystem/model/base/InfoSystemFactory.java
@@ -0,0 +1,79 @@
+/**
+ */
+package eu.celar.infosystem.model.base;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see eu.celar.infosystem.model.base.InfoSystemPackage
+ * @generated
+ */
+public interface InfoSystemFactory extends EFactory
+{
+  /**
+   * The singleton instance of the factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  InfoSystemFactory eINSTANCE = eu.celar.infosystem.model.base.impl.InfoSystemFactoryImpl.init();
+
+  /**
+   * Returns a new object of class '<em>Monitoring Probe</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Monitoring Probe</em>'.
+   * @generated
+   */
+  MonitoringProbe createMonitoringProbe();
+
+  /**
+   * Returns a new object of class '<em>Resizing Action</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Resizing Action</em>'.
+   * @generated
+   */
+  ResizingAction createResizingAction();
+
+  /**
+   * Returns a new object of class '<em>Software Dependency</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Software Dependency</em>'.
+   * @generated
+   */
+  SoftwareDependency createSoftwareDependency();
+
+  /**
+   * Returns a new object of class '<em>Virtual Machine Image</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Virtual Machine Image</em>'.
+   * @generated
+   */
+  VirtualMachineImage createVirtualMachineImage();
+
+  /**
+   * Returns a new object of class '<em>User Application</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>User Application</em>'.
+   * @generated
+   */
+  UserApplication createUserApplication();
+
+  /**
+   * Returns the package supported by this factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the package supported by this factory.
+   * @generated
+   */
+  InfoSystemPackage getInfoSystemPackage();
+
+} //InfoSystemFactory
---
+++ app-description/eu.celar.infosystem.model/src/eu/celar/infosystem/model/base/impl/InfoSystemFactoryImpl.java
@@ -0,0 +1,235 @@
+/**
+ */
+package eu.celar.infosystem.model.base.impl;
+
+import eu.celar.infosystem.model.base.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class InfoSystemFactoryImpl extends EFactoryImpl implements InfoSystemFactory
+{
+  /**
+   * Creates the default factory implementation.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static InfoSystemFactory init()
+  {
+    try
+    {
+      InfoSystemFactory theInfoSystemFactory = (InfoSystemFactory)EPackage.Registry.INSTANCE.getEFactory("http://www.example.org/InfoSystem"); 
+      if (theInfoSystemFactory != null)
+      {
+        return theInfoSystemFactory;
+      }
+    }
+    catch (Exception exception)
+    {
+      EcorePlugin.INSTANCE.log(exception);
+    }
+    return new InfoSystemFactoryImpl();
+  }
+
+  /**
+   * Creates an instance of the factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public InfoSystemFactoryImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public EObject create(EClass eClass)
+  {
+    switch (eClass.getClassifierID())
+    {
+      case InfoSystemPackage.MONITORING_PROBE: return createMonitoringProbe();
+      case InfoSystemPackage.RESIZING_ACTION: return createResizingAction();
+      case InfoSystemPackage.SOFTWARE_DEPENDENCY: return createSoftwareDependency();
+      case InfoSystemPackage.VIRTUAL_MACHINE_IMAGE: return createVirtualMachineImage();
+      case InfoSystemPackage.USER_APPLICATION: return createUserApplication();
+      default:
+        throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+    }
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object createFromString(EDataType eDataType, String initialValue)
+  {
+    switch (eDataType.getClassifierID())
+    {
+      case InfoSystemPackage.VIRTUAL_MACHINE_IMAGE_TYPE:
+        return createVirtualMachineImageTypeFromString(eDataType, initialValue);
+      case InfoSystemPackage.VIRTUAL_MACHINE_IMAGE_TYPE_OBJECT:
+        return createVirtualMachineImageTypeObjectFromString(eDataType, initialValue);
+      default:
+        throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+    }
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public String convertToString(EDataType eDataType, Object instanceValue)
+  {
+    switch (eDataType.getClassifierID())
+    {
+      case InfoSystemPackage.VIRTUAL_MACHINE_IMAGE_TYPE:
+        return convertVirtualMachineImageTypeToString(eDataType, instanceValue);
+      case InfoSystemPackage.VIRTUAL_MACHINE_IMAGE_TYPE_OBJECT:
+        return convertVirtualMachineImageTypeObjectToString(eDataType, instanceValue);
+      default:
+        throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+    }
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public MonitoringProbe createMonitoringProbe()
+  {
+    MonitoringProbeImpl monitoringProbe = new MonitoringProbeImpl();
+    return monitoringProbe;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public ResizingAction createResizingAction()
+  {
+    ResizingActionImpl resizingAction = new ResizingActionImpl();
+    return resizingAction;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public SoftwareDependency createSoftwareDependency()
+  {
+    SoftwareDependencyImpl softwareDependency = new SoftwareDependencyImpl();
+    return softwareDependency;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public VirtualMachineImage createVirtualMachineImage()
+  {
+    VirtualMachineImageImpl virtualMachineImage = new VirtualMachineImageImpl();
+    return virtualMachineImage;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public UserApplication createUserApplication()
+  {
+    UserApplicationImpl userApplication = new UserApplicationImpl();
+    return userApplication;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public VirtualMachineImageType createVirtualMachineImageTypeFromString(EDataType eDataType, String initialValue)
+  {
+    VirtualMachineImageType result = VirtualMachineImageType.get(initialValue);
+    if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+    return result;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String convertVirtualMachineImageTypeToString(EDataType eDataType, Object instanceValue)
+  {
+    return instanceValue == null ? null : instanceValue.toString();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public VirtualMachineImageType createVirtualMachineImageTypeObjectFromString(EDataType eDataType, String initialValue)
+  {
+    return createVirtualMachineImageTypeFromString(InfoSystemPackage.Literals.VIRTUAL_MACHINE_IMAGE_TYPE, initialValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String convertVirtualMachineImageTypeObjectToString(EDataType eDataType, Object instanceValue)
+  {
+    return convertVirtualMachineImageTypeToString(InfoSystemPackage.Literals.VIRTUAL_MACHINE_IMAGE_TYPE, instanceValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public InfoSystemPackage getInfoSystemPackage()
+  {
+    return (InfoSystemPackage)getEPackage();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @deprecated
+   * @generated
+   */
+  @Deprecated
+  public static InfoSystemPackage getPackage()
+  {
+    return InfoSystemPackage.eINSTANCE;
+  }
+
+} //InfoSystemFactoryImpl
