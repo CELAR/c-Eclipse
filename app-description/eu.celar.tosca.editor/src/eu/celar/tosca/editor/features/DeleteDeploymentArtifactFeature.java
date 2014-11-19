e972a45c0d445fb11436c91dba65d9a6abcf8291
stalosof <stalo@cs8526.cs.ucy.ac.cy>
2014-11-19 16:25:51 +0200

    Fixed DeleteDeploymentArtifactFeature

--- app-description/eu.celar.tosca.editor/src/eu/celar/tosca/editor/features/DeleteDeploymentArtifactFeature.java
+++ app-description/eu.celar.tosca.editor/src/eu/celar/tosca/editor/features/DeleteDeploymentArtifactFeature.java
@@ -1,12 +1,17 @@
 package eu.celar.tosca.editor.features;
 
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IDeleteContext;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.ui.features.DefaultDeleteFeature;
 
+import eu.celar.tosca.TArtifactTemplate;
+import eu.celar.tosca.TDeploymentArtifact;
 import eu.celar.tosca.TNodeTemplate;
+import eu.celar.tosca.editor.ModelHandler;
+import eu.celar.tosca.editor.ToscaModelLayer;
 
 public class DeleteDeploymentArtifactFeature extends DefaultDeleteFeature{
   
@@ -28,6 +33,19 @@ public class DeleteDeploymentArtifactFeature extends DefaultDeleteFeature{
     if ( parentNodeTemplate.getDeploymentArtifacts().getDeploymentArtifact().size() == 1 ){
       this.deleteDeploymentArtifact = true;
       this.parentNodeTemplate = parentNodeTemplate;
+    }
+    
+    //Delete corresponding TArtifactTemplate
+    Object deletedObject = getFeatureProvider().getBusinessObjectForPictogramElement( context.getPictogramElement() );
+    TDeploymentArtifact deletedDeploymentArtifact = (TDeploymentArtifact) deletedObject;
+    String deletedDeploymentArtifactId = deletedDeploymentArtifact.getArtifactRef().toString();
+    
+    final ToscaModelLayer model = ModelHandler.getModel( EcoreUtil.getURI( getDiagram() ) );
+    
+    for ( TArtifactTemplate tempArtifactTemplate : model.getDocumentRoot().getDefinitions().getArtifactTemplate() ){
+      if (tempArtifactTemplate.getId().compareTo( deletedDeploymentArtifactId )==0){
+        model.getDocumentRoot().getDefinitions().getArtifactTemplate().remove( tempArtifactTemplate );
+      }
     }
   }
   
